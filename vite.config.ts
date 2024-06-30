aab6764950e402c0c08a257d2022573dd7ef3a5e
Kelsey <kelsey@loop.com>
2024-06-30 17:32:06 -0500

    add router basename

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/siman-incentives/",
   plugins: [
     remix({
+      basename: "/siman-incentives",
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
