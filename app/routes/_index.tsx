c5e03c13cf5e0410ff6c568e7b3abd486b085a45
Kelsey <kelsey@loop.com>
2024-06-30 16:53:08 -0500

    add MUI AppBar shell

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,48 +1,24 @@
+import { AppBar, Box, Container, MenuItem, Toolbar, Typography } from "@mui/material";
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Siman Incentives" },
+    { name: "description", content: "Welcome to the website for Siman Incentives" },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+      <AppBar position="fixed">
+        <Container maxWidth="xl">
+          <Toolbar variant="regular">
+            <Box alignItems="center" display="flex" flexGrow={1} justifyContent="space-between">[Logo]
+          <Box display="flex" gap={2}>
+            <MenuItem><Typography>Products</Typography></MenuItem>
+          <MenuItem><Typography>Contact</Typography></MenuItem>
+          </Box></Box></Toolbar>
+        </Container>
+      </AppBar>
   );
 }
